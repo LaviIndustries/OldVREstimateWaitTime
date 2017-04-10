6fb893582d313d41ca8cffa4d7dd231a9b0ebd10
Hai Wang <haiw@lavi.com>
2017-04-10 14:34:08 -0700

    Added project installer

--- WaitTimeSimulatorWindowsS/Properties/AssemblyInfo.cs
+++ WaitTimeSimulatorWindowsS/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WaitTimeSimulatorWindowsS")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Wait Time Simulator Windows Service")]
+[assembly: AssemblyDescription("Generating Estimate Wait Time (Branch based)")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WaitTimeSimulatorWindowsS")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCompany("Lavi Industries")]
+[assembly: AssemblyProduct("Wait Time Simulator Windows Service")]
+[assembly: AssemblyCopyright("Copyright © Lavi Industries 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
